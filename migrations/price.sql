fd53e1694c4d6644622477309f13b08d5244ce29
six7zero9 <jacksons@Michaels-MacBook-Pro.local>
2014-07-16 11:35:59 -0500

    int to dec

--- migrations/price.sql
+++ migrations/price.sql
@@ -3,10 +3,11 @@ CREATE TABLE `price` (
   `desc` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '',
   `quantity` int(11) unsigned DEFAULT NULL,
   `date` date DEFAULT NULL,
-  `price` int(11) unsigned DEFAULT NULL,
-  `buylist` int(11) unsigned DEFAULT NULL,
+  `price` decimal(11,2) unsigned DEFAULT NULL,
+  `buylist` decimal(11,2) unsigned DEFAULT NULL,
   `card_id` int(11) unsigned DEFAULT NULL,
   `vendor_id` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`card_id`) REFERENCES cards(`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+  KEY `card_id` (`card_id`),
+  CONSTRAINT `price_ibfk_1` FOREIGN KEY (`card_id`) REFERENCES `cards` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
